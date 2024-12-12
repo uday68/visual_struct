1a6ea10daf1a7f4842a3617a29ee7bc3c44b2ad3
uday <vuday8370@gmail.com>
2024-12-12 20:12:55 +0530

    ad
    dsaf

--- ds/ds/settings.py
+++ ds/ds/settings.py
@@ -44,6 +44,7 @@ DEBUG = True
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -51,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = "ds.urls"
 
 TEMPLATES = [
